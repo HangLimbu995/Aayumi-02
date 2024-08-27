97173921367d9c8d4d36aa2a81f2183986471bae
Hang Limbu <77398461+khesehang@users.noreply.github.com>
2024-08-27 16:29:49 +0545

    navbar re-make

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,33 +8,37 @@ const Navbar = () => {
     const [dropDown, setDropDown] = useState(false)
 
     return (
-        <div className='w-full h-[8vh] bg-red-900 grid place-items-center'>
-            <div className='w-[90%] md:w-[80%] h-full flex justify-between items-center'>
+        <nav className='w-full  bg-red-900 grid place-items-center'>
+            <div className='w-[90%] md:w-[80%] h-full flex justify-between items-center  py-2'>
                 <div style={{ zIndex: 10 }}>
                     <h1 className='text-3xl font-bold' >logo</h1>
                 </div>
-                <div className='flex gap-9'>
-                    <ul className='gap-9 hidden md:flex'>
+                <div className='flex gap-9 h-full items-center '>
+                    <ul className='gap-9 hidden xl:flex items-center h-full'>
                         <li>Home</li>
-                        <li className='item relative'>
-                            <p>Service</p>
-                            <ul className='submenu absolute bg-pink-500 py-4 px-6 transform translate-x-[-20%] mt-3 flex-col gap-5 text-[1.1em] font-[400]'>
-                                <li>item</li>
-                                <li>item</li>
-                                <li>item</li>
+                        <li className='nav-item relative'>
+                            <a href='#' className='h-full flex items-center'>Service  &nbsp;   <IoIosArrowUp className='nav-icon' /></a>
+                            <ul className='submenu absolute w-[20vw] bg-white  transform translate-x-[-20%] flex flex-col gap-3  top-9 shadow-md' style={{ zIndex: 2 }}>
+                                <li>enhanced design</li>
+                                <li>budjet estimation</li>
+                                <li>raw material cost</li>
+                                <li>bridge construction</li>
+                                <li>electricity pole construction</li>
+                                <li>road construction</li>
                             </ul>
                         </li>
+
                         <li>About</li>
                         <li>Contact</li>
                     </ul>
-                    <button onClick={() => setSlideMenu(!slideMenu)} className='flex md:hidden' style={{ zIndex: 10 }}>
+                    <button onClick={() => setSlideMenu(!slideMenu)} className='flex xl:hidden' style={{ zIndex: 10 }}>
                         {
                             !slideMenu ? <FaBars size={33} /> : <FaTimes size={33} />
                         }
                     </button>
                 </div>
                 {slideMenu ? (
-                    <div className='slide-menu fixed top-0 left-0 w-full h-[100vh] bg-red-900' style={{zIndex:9}}>
+                    <div className='slide-menu fixed top-0 right-0 max-w-[500px] w-full h-[100vh] bg-red-900' style={{ zIndex: 9 }}>
                         <div className=' w-full h-full  mt-[50px] p-6'>
                             <div className='w-full h-full '>
                                 <ul className=' text-[26px] flex flex-col gap-5'>
@@ -61,7 +65,7 @@ const Navbar = () => {
                     </div>
                 ) : ''}
             </div>
-        </div>
+        </nav>
     )
 }
 
